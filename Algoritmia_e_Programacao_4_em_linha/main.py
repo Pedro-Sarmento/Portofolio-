82303cf23e2b92a3286235e01a4a2e0e048d24be
Pedro Sarmento <pedroasarmento@hotmail.com>
2023-01-29 16:24:24 +0000

    2nd

--- Algoritmia_e_Programacao_4_em_linha/main.py
+++ Algoritmia_e_Programacao_4_em_linha/main.py
@@ -2,4 +2,4 @@ from view import GUI
 
 
 if __name__ == "__main__":
-    GUI.gui()
+    GUI.App()
--- Algoritmia_e_Programacao_4_em_linha/view/GUI.py
+++ Algoritmia_e_Programacao_4_em_linha/view/GUI.py
@@ -3,25 +3,24 @@ import controllers.gamefunctions as game_fuc
 import controllers.userfunctions as user_fuc
 
 
-def gui():
-    ctk.set_appearance_mode("dark")
-    ctk.set_default_color_theme("green")
-    root = ctk.CTk()
-    root.geometry("500x300")
-    login_button = ctk.CTkButton(master=root, text="Login", command=login_interface(root))
-    login_button.pack(pady=12, padx=10)
-    root.mainloop()
+class App:
+    def __init__(self):
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("green")
+        self.root = ctk.CTk()
+        self.root.geometry("500x300")
+        self.login_button = ctk.CTkButton(master=self.root, text="Login", command=lambda: [self.login_interface(self.root), self.login_button.forget()])
+        self.login_button.pack(pady=12, padx=10)
+        self.root.mainloop()
 
-
-def login_interface(master):
-    login_frame = ctk.CTkFrame(master= master)
-    login_frame.pack(pady=20, padx=60, fill="both", expand=True)
-    login_label = ctk.CTkLabel(master=login_frame, text="Login System")
-    login_label.pack(pady=12, padx=10)
-    login_username_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Username")
-    login_username_entry.pack(pady=12, padx=10)
-    login_password_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Password", show="*")
-    login_password_entry.pack(pady=12, padx=10)
-    login_submit_button = ctk.CTkButton(master=login_frame, text="Login", command=user_fuc.submit_login())
-    login_submit_button.pack(pady=12, padx=10)
-
+    def login_interface(self, master):
+        login_frame = ctk.CTkFrame(master=master)
+        login_frame.pack(pady=20, padx=60, fill="both", expand=True)
+        login_label = ctk.CTkLabel(master=login_frame, text="Login System")
+        login_label.pack(pady=12, padx=10)
+        login_username_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Username")
+        login_username_entry.pack(pady=12, padx=10)
+        login_password_entry = ctk.CTkEntry(master=login_frame, placeholder_text="Password", show="*")
+        login_password_entry.pack(pady=12, padx=10)
+        login_submit_button = ctk.CTkButton(master=login_frame, text="Login", command=user_fuc.submit_login())
+        login_submit_button.pack(pady=12, padx=10)
